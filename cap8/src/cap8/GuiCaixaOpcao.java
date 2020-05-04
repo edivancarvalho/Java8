851e13310c07f1e4d56868b0f5858c0827a410d4
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-05-04 11:36:42 -0300

    Update GuiCaixaOpcao.java

--- cap8/src/cap8/GuiCaixaOpcao.java
+++ cap8/src/cap8/GuiCaixaOpcao.java
@@ -1,7 +1,58 @@
 package cap8;
 import java.awt.event.*;
 import javax.swing.*;
-
 public class GuiCaixaOpcao extends JPanel{
-    private 
+    private JButton btVerificar, btMarcar, btDesmarcar;
+    private JCheckBox ckIngles, ckInformatica;
+    
+    public GuiCaixaOpcao(){
+        inicializarComponestes();
+        definirEventos();
+    }
+    
+    private void inicializarComponestes() {
+        btVerificar = new JButton("Verificar");
+        ckIngles = new JCheckBox("Ingles");
+        ckInformatica = new JCheckBox("Informatica");
+        btMarcar = new JButton("Marcar");
+        btDesmarcar = new JButton("Desmarcar");
+        setLayout(null);
+        add(btVerificar);
+        add(ckIngles);
+        add(ckInformatica);
+        add(btMarcar);
+        add(btDesmarcar);
+        btVerificar.setBounds(20,70,100,20);
+        ckIngles.setBounds(15,15,100,25);
+        ckInformatica.setBounds(15,40,100,25);
+        btMarcar.setBounds(20,100,100,20);
+        btDesmarcar.setBounds(20,130,100,20);
+    }
+
+    private void definirEventos() {
+        btMarcar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ckInformatica.setSelected(false);
+                ckIngles.setSelected(false);
+            }
+        });
+        btDesmarcar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ckInformatica.setSelected(false);
+                ckIngles.setSelected(false);
+            }
+        });
+        btVerificar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {    
+                String selecao = "selecionados: ";
+                if (ckIngles.isSelected()){
+                    selecao += "\nInglês";
+                }
+                if (ckInformatica.isSelected()){
+                    selecao += "\nInformática";
+                }
+                JOptionPane.showMessageDialog(null, selecao);
+            }
+        });       
+    }
 }
