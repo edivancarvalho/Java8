657b69bac1098b4b17f94d81a632cb32a415ed11
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-05-04 09:03:33 -0300

    Class de Login
    Esta classe de refere a tela de login e modo gráfico.

--- cap8/src/cap8/GuiLogin.java
+++ cap8/src/cap8/GuiLogin.java
@@ -1,17 +1,9 @@
 package cap8;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class GuiLogin extends JFrame {
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-
-/**
- *
- * @author suporte
- */
-public class GuiLogin extends JFrame{
-    
     private JTextField tflogin;
     private JLabel lbSenha;
     private JLabel lbLogin;
@@ -19,10 +11,10 @@ public class GuiLogin extends JFrame{
     private JButton btCancelar;
     private JPasswordField pfSenha;
     private static GuiLogin frame;
-    
-    public GuiLogin(){
+
+    public GuiLogin() {
         inicializarComponetes();
-        definireventos();
+        definirEventos();
     }
 
     private void inicializarComponetes() {
@@ -33,22 +25,52 @@ public class GuiLogin extends JFrame{
         pfSenha = new JPasswordField(5);
         lbSenha = new JLabel("senha:");
         lbLogin = new JLabel("Login:");
-        btLogar = new JButton("Logar:");
+        btLogar = new JButton("Logar");
         btCancelar = new JButton("Cancelar");
         tflogin.setBounds(100, 30, 120, 25);
         lbLogin.setBounds(30, 30, 80, 25);
         lbSenha.setBounds(30, 75, 80, 25);
+        pfSenha.setBounds(100, 75, 120, 25);
         btLogar.setBounds(20, 120, 100, 25);
-        btCancelar.setBounds(125, 120, 200, 25);
+        btCancelar.setBounds(125, 120, 100, 25);
         add(tflogin);
         add(lbSenha);
         add(lbLogin);
         add(btLogar);
         add(btCancelar);
         add(pfSenha);
-        
     }
-    
-    
-    
+
+    private void definirEventos() {
+        btLogar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String senha = String.valueOf(pfSenha.getPassword());
+                if (tflogin.getText().equals("java8") && senha.equals("java8")) {
+                    frame.setVisible(false);
+                    //GuiMenuPrincipal.abrir();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Login ou Senha incorretas");
+                }
+            }
+        });
+
+        btCancelar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                frame = new GuiLogin();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+                frame.setLocation((tela.width - frame.getSize().width) / 2,
+                        (tela.height - frame.getSize().height) / 2);
+                frame.setVisible(true);
+            }
+        });
+    }
 }
